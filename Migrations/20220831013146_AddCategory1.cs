75b858017112d105c651544603cada2ded9e9519
Rolando Penagos <rpmacal2013@outlook.com>
2022-09-05 18:32:56 -0500

    Versión inicial

---
+++ Migrations/20220831013146_AddCategory1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class AddCategory1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DsiaplyOrder",
+                table: "Categories",
+                newName: "DisplayOrder");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "Categories",
+                newName: "DsiaplyOrder");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Orden a mostrar")]
+        [Range(1,100, ErrorMessage ="El valor debe estar entre 1 y 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
+ 
